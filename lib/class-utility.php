4daca154610939ff6a2ec6410cf574334e11b0cc
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-01-10 15:28:22 +0200

    added Utility class

---
+++ lib/class-utility.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Help class
+ *
+ * @author potanin@UD
+ * @author peshkov@UD
+ * @author korotkov@UD
+ *
+ * @version 0.1.0
+ * @package UsabilityDynamics
+ * @subpackage Shortcode
+ */
+namespace UsabilityDynamics\Shortcode {
+
+  if( !class_exists( 'UsabilityDynamics\Shortcode\Utility' ) ) {
+
+    class Utility {
+
+      /**
+       * Parses passed directory for shortcodes files
+       * and includes and adds shortcodes if they exist
+       * 
+       * @param $path
+       * @author peshkov@UD
+       */
+      static public function maybe_load_shortcodes( $path ) {
+        
+      }
+    
+    }
+
+  }
+
+}
