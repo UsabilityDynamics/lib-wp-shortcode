37997441d34fa2d41572e153b0e9f807fbbfa720
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-01-13 14:59:11 +0200

    added cache functionality to maybe_load_shortcodes

--- lib/class-utility.php
+++ lib/class-utility.php
@@ -20,15 +20,31 @@ namespace UsabilityDynamics\Shortcode {
        * Parses passed directory for shortcodes files
        * includes and adds shortcodes if they exist
        * 
-       * @param $path
+       * @param string $path
+       * @param boolean $cache
        * @author peshkov@UD
        */
-      static public function maybe_load_shortcodes( $path ) {
+      static public function maybe_load_shortcodes( $path, $cache = true ) {
         if ( !is_dir( $path ) ) {
           return null;
         }
         
-        if ( $dir = opendir( $path ) ) {
+        $_shortcodes = wp_cache_get( 'shortcodes', 'usabilitydynamics' );
+        if( !is_array( $_shortcodes ) ) {
+          $_shortcodes = array();
+        }
+        
+        if( $cache && !empty( $_shortcodes[ $path ] ) && is_array( $_shortcodes[ $path ] ) ) {
+          foreach( $_shortcodes[ $path ] as $_shortcode ) {
+            include_once( $path . "/" . $_shortcode[ 'file' ] );
+            new $_shortcode[ 'headers' ][ 'class' ]();
+          }
+          return null;
+        }
+        
+        $_shortcodes[ $path ] = array();
+        
+        if ( $dir = @opendir( $path ) ) {
           $headers = array(
             'name' => 'Name',
             'id' => 'ID',
@@ -40,16 +56,21 @@ namespace UsabilityDynamics\Shortcode {
           );
           while ( false !== ( $file = readdir( $dir ) ) ) {
             $data = @get_file_data( $path . "/" . $file, $headers, 'shortcode' );
-            //die($data[ 'class' ]);
             if( $data[ 'type' ] == 'shortcode' && !empty( $data[ 'class' ] ) ) {
               include_once( $path . "/" . $file );
               if( class_exists( $data[ 'class' ] ) ) {
+                array_push( $_shortcodes[ $path ], array(
+                  'file' => $file,
+                  'headers' => $data,
+                ) );
                 new $data[ 'class' ]();
               }
             }
-            //echo "<pre>"; echo $data[ 'class' ]; var_dump( class_exists( $data[ 'class' ] ) ); echo "</pre>";
           }
         }
+        
+        wp_cache_set( 'shortcodes', $_shortcodes, 'usabilitydynamics' );
+        
       }
     
     }
